19b6b198823afe9e6618e9d3ff47bbeb4a638512
jhandy4 <58635628+jhandy4@users.noreply.github.com>
2020-05-12 14:30:38 -0400

    commit

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,13 +4,7 @@ const FILES_TO_CACHE = [
   "/index.js",
   "/manifest.webmanifest",
   "/style.css",
-  "/icons/icon-72x72.png",
-  "/icons/icon-96x96.png",
-  "/icons/icon-128x128.png",
-  "/icons/icon-144x144.png",
-  "/icons/icon-152x152.png",
   "/icons/icon-192x192.png",
-  "/icons/icon-384x384.png",
   "/icons/icon-512x512.png"
 ];
 
@@ -51,21 +45,24 @@ self.addEventListener("activate", function(evt) {
 self.addEventListener("fetch", function(evt) {
   if (evt.request.url.includes("/api/")) {
     evt.respondWith(
-      caches.open(DATA_CACHE_NAME).then(cache => {
-        return fetch(evt.request)
-          .then(response => {
-            // If the response was good, clone it and store it in the cache.
-            if (response.status === 200) {
-              cache.put(evt.request.url, response.clone());
-            }
+      caches
+        .open(DATA_CACHE_NAME)
+        .then(cache => {
+          return fetch(evt.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
 
-            return response;
-          })
-          .catch(err => {
-            // Network request failed, try to get it from the cache.
-            return cache.match(evt.request);
-          });
-      }).catch(err => console.log(err))
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        })
+        .catch(err => console.log(err))
     );
 
     return;
